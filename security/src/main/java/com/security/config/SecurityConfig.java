b822ad7f4d69d0e7b8fca23b70f58354e58b4ebf
Vishal Tomar <vishaltomar004@gmail.com>
2025-03-26 00:00:07 +0530

    Basic spring securiyDone with securityFilterChain methods

---
+++ security/src/main/java/com/security/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf( customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+
+        // making session stateless . the csrf thing
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+
+        // BDS nehind the scenes
+
+
+        return http.build();
+    }
+}
